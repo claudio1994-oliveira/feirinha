aa622380988cbda82db8e8061efa047dd70c142e
Claudio Oliveira <claudiooliveira@aridesa.com.br>
2025-09-12 17:05:23 -0300

    remover ícone do cabeçalho e substituir por título estilizado na página de gerenciamento de feirinhas

--- resources/views/livewire/fairs/manage.blade.php
+++ resources/views/livewire/fairs/manage.blade.php
@@ -6,12 +6,7 @@
         <div class="flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
             <div class="space-y-2">
                 <div class="flex items-center gap-3">
-                    <div class="p-2 bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg">
-                        <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
-                        </svg>
-                    </div>
+
                     <h1
                         class="text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 dark:from-blue-400 dark:to-purple-400 bg-clip-text text-transparent">
                         🏪 Gerenciar: {{ $fair->name }}
